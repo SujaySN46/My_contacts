554f632e71a097a0939aac8fe3cf8d9443f7a592
Sujay S N <80213368+SujaySN46@users.noreply.github.com>
2021-03-12 13:57:17 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Keanu_Reeves_(crop_and_levels)_(cropped).jpg/1200px-Keanu_Reeves_(crop_and_levels)_(cropped).jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
